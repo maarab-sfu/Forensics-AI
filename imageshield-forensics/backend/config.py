a423ea99f10d16c2a5fb15551a244dce7dce092b
Mohammad-Amin Arab <aminarab@ymail.com>
2025-12-26 21:28:04 -0800

    Update config.py

--- imageshield-forensics/backend/config.py
+++ imageshield-forensics/backend/config.py
@@ -12,7 +12,7 @@ Counter2 = 14000
 
 
 
-WM_MODEL = "InvisMark" # InvisMark or TrustMark or RedMark
+WM_MODEL = "None" # InvisMark or TrustMark or RedMark
 HashEmbedding = False
 ECCMethod = 'None'  # None or 'BCH' or 'RS' or 'polar'
 
